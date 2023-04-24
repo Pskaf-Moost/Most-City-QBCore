93a4f64c47b41a49a957b1ae7b0c21a3d3698471
Moost <moost.tv@gmail.com>
2023-04-24 22:09:20 +0200

    Change Pawn Price + Taxi Price + Recquired Cops for jewelry robs

--- [qb]/qb-pawnshop/config.lua
+++ [qb]/qb-pawnshop/config.lua
@@ -24,35 +24,35 @@ Config.UseTarget = GetConvar('UseTarget', 'false') == 'true'
 Config.PawnItems = {
     [1] = {
         item = 'goldchain',
-        price = math.random(50,100)
+        price = math.random(300,400)
     },
     [2] = {
         item = 'diamond_ring',
-        price = math.random(50,100)
+        price = math.random(300,400)
     },
     [3] = {
         item = 'rolex',
-        price = math.random(50,100)
+        price = math.random(300,400)
     },
     [4] = {
         item = '10kgoldchain',
-        price = math.random(50,100)
+        price = math.random(300,600)
     },
     [5] = {
         item = 'tablet',
-        price = math.random(50,100)
+        price = math.random(300,400)
     },
     [6] = {
         item = 'iphone',
-        price = math.random(50,100)
+        price = math.random(300,400)
     },
     [7] = {
         item = 'samsungphone',
-        price = math.random(50,100)
+        price = math.random(300,400)
     },
     [8] = {
         item = 'laptop',
-        price = math.random(50,100)
+        price = math.random(300,400)
     }
 }
 
