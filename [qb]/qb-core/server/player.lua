d7c7fb012ed2e012a625cf1bd8936af07565d84e
Moost <moost.tv@gmail.com>
2023-04-30 11:47:15 +0200

    Remove RS6 + Phone number = 06 - 1000 - 9999

--- [qb]/qb-core/server/player.lua
+++ [qb]/qb-core/server/player.lua
@@ -642,7 +642,8 @@ function QBCore.Functions.CreatePhoneNumber()
     local UniqueFound = false
     local PhoneNumber = nil
     while not UniqueFound do
-        PhoneNumber = math.random(100,999) .. math.random(1000000,9999999)
+       -- PhoneNumber = math.random(100,999) .. math.random(1000000,9999999)
+        PhoneNumber = 06 .. math.random(1000,9999) 
         local query = '%' .. PhoneNumber .. '%'
         local result = MySQL.prepare.await('SELECT COUNT(*) as count FROM players WHERE charinfo LIKE ?', { query })
         if result == 0 then
